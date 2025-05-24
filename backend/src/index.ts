69f07d7dc446db663f31b8bbc13ae7f492b0b8ab
RadiALo <78644882+RadiALo@users.noreply.github.com>
2025-05-24 21:29:34 +0300

    mongoose initialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,22 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI || "";
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Сервер запущено на http://localhost:${PORT}`);
-});
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log('✅ Підключено до MongoDB Atlas');
+    app.listen(port, () => {
+      console.log(`🚀 Сервер слухає на http://localhost:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('❌ Помилка підключення до MongoDB:', err);
+  });
