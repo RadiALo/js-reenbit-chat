a0defd243c6a9526d570ab425e74b02368e535bd
RadiALo <78644882+RadiALo@users.noreply.github.com>
2025-05-24 22:10:43 +0300

    delete redundant field

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,7 +1,6 @@
 import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
-  uuid: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
