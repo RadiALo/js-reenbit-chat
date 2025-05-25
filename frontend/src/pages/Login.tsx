86a64c54cf1ecaff8c54beb2f09aba8e442f49af
RadiALo <78644882+RadiALo@users.noreply.github.com>
2025-05-25 10:58:40 +0300

    links to login-register

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LoginForm from '../forms/LoginForm';
 
 const Register: React.FC = () => {
@@ -6,6 +7,9 @@ const Register: React.FC = () => {
     <div>
       <h1>Login Page</h1>
       <LoginForm />
+      <p>
+        Don't have an account? <Link to='/register'>Register here</Link>.
+      </p>
     </div>
   );
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import RegisterForm from '../forms/RegisterForm';
+import { Link } from "react-router-dom";
 
 const Register: React.FC = () => {
   return (
     <div>
       <h1>Register Page</h1>
       <RegisterForm />
+      <p>
+        Already have an account? <Link to='/login'>Login here</Link>.
+      </p>
     </div>
   );
 }
