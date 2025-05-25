c8ab7fdb44e8c97792242d01cf58a6e5d664079e
RadiALo <78644882+RadiALo@users.noreply.github.com>
2025-05-25 10:58:59 +0300

    Revert "Frontend init"

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { UserRouter } from './routers/user.router';
@@ -9,13 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoUri = process.env.MONGO_URI || "";
-const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
-
 const userRouter = new UserRouter();
-
-app.use(cors({
-  origin: frontendUrl
-}));
 
 app.use(express.json());
 app.use('/api/users', userRouter.router);
