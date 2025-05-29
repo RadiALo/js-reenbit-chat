372bffe9e5e03c20c49c585993357a540e22c017
RadiALo <78644882+RadiALo@users.noreply.github.com>
2025-05-29 22:35:46 +0300

    better ChatEbtry date

--- frontend/src/components/ChatEntry/ChatEntry.tsx
+++ frontend/src/components/ChatEntry/ChatEntry.tsx
@@ -4,19 +4,25 @@ import React from "react";
 type ChatEntryProps = {
   name: string;
   message: string;
-  date: string;
+  date?: Date;
   onClick?: () => void;
 };
 
 const ChatEntry: React.FC<ChatEntryProps> = ({ name, message, date, onClick }) => {
   return (
     <div onClick={onClick} className="chats--item">
+
       <img className="user-icon" src="/user-icon.png" alt="User icon" />
       <div className="chats--item--text">
         <p className="chats--item--text--name">{name}</p>
         <p className="chats--item--text--message">{message}</p>
       </div>
-      <div className="chats--item--date">{date}</div>
+
+      {date && <div className="chats--item--date">{new Date(date).toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+      })}</div>}
     </div>
   );
 };
